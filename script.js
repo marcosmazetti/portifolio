cbe336f03eeccdea296a07fb5638f4aace6ed906
marcosmazetti <marcos.mazetti@escola.pr.gov.br>
2025-11-17 19:27:17 -0300

    condicional

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let texto = document.querySelector("#texto");
 texto.textContent = "Marcos";
 let nome = prompt("Qual é o seu nome?");
+if(nome==null){
+    texto.textContent = 'Seja bem-vindo';
+}else{
 texto.textContent = nome;
+}
