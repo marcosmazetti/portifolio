4c2ca1efdf6f8fd2d9c132cc84071f03b3ef6c9d
marcosmazetti <marcos.mazetti@escola.pr.gov.br>
2025-11-17 19:39:39 -0300

    if

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let texto = document.querySelector("#texto");
 texto.textContent = "Marcos";
 let nome = prompt("Qual é o seu nome?");
 if(nome==null){
-    texto.textContent = 'Seja bem-vindo';
+    texto.textContent = "Seja bem-vindo";
 }else{
 texto.textContent = nome;
 }
